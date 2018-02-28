a9d2ee62bbc65913b67025adde169c518c43837e
Yuki Hirai <yukihirai0505@gmail.com>
2018-02-28 17:42:55 +0900

    Update MemoryArrays

--- examples/MemoryArrays.sol
+++ examples/MemoryArrays.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.0;
 contract MemoryArrays {
 	function f(uint len){
 		// uintの動的配列を7つ作成する
-		uint[] memory a = new uint[][7];
+		uint[] memory a = new uint[](7);
 		// サイズがlenの固定バイト配列を作成する
 		bytes memory b = new bytes(len);
 		a[6] = 8;
