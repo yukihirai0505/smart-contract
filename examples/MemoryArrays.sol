904df00c353bea3f4abd5dfcc569d917a3951cd3
Yuki Hirai <yukihirai0505@gmail.com>
2018-02-28 17:40:17 +0900

    Add Arrays and MemoryArrays

---
+++ examples/MemoryArrays.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.0;
+
+contract MemoryArrays {
+	function f(uint len){
+		// uintの動的配列を7つ作成する
+		uint[] memory a = new uint[][7];
+		// サイズがlenの固定バイト配列を作成する
+		bytes memory b = new bytes(len);
+		a[6] = 8;
+	}
+}
